64ac964958a4f785714ad63d63941dbc1cea678c
JuanFeBlancoT <72984915+JuanFeBlancoT@users.noreply.github.com>
2021-03-22 20:36:04 -0500

    All functional requirements implemented and working correclty

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+
 import processing.core.PApplet;
 
 public class Main extends PApplet{
@@ -6,18 +8,24 @@ public class Main extends PApplet{
 		PApplet.main("Main");
 	}
 	
+	int spawnTime, index, buttonControl;
+	private Palabra[] wordsToMatch;
 	private String[] textS;
 	private String[] lines;
 	private ArrayList<String> words;
 	private ArrayList<Palabra> palabritas;
 	
 	public void settings() {
-		size(400,400);
+		size(1000,900);
 	}
 	
 	public void setup() {
+		spawnTime = 0;
+		index = 0;
+		buttonControl=0;
 		textSize(20);
 		textS=loadStrings("strings.txt");
+		wordsToMatch = new Palabra[4];
 		words = new ArrayList<>();
 		palabritas = new ArrayList<>();
 		
@@ -29,19 +37,127 @@ public class Main extends PApplet{
 			}
 		}
 		
-		for (int i = 0; i < words.size(); i++) {
-			palabritas.add(new Palabra(words.get(i)));
-		}
-		
+			for (int i = 0; i < words.size(); i++) {
+				palabritas.add(new Palabra(words.get(i)));
+			}
+			
+		wordsToMatch = selectWords();
+		//System.out.println(wordsToMatch[0].getPalabra());
 	}
 
 	public void draw() {
+		
 		background(250);
+		fill(0);
 		for (int i = 0; i <palabritas.size() ; i++) {
 			//palabritas.get(i).crearPalabra();
 			palabritas.get(i).drawP(this);
 			palabritas.get(i).mover();
 		}
 		
+		fill(220);
+		rect(500, 750, 500, 300);
+		
+		for (int i = 0; i < wordsToMatch.length; i++) {
+			
+			if(wordsToMatch[i].isSelected()==false) {
+				wordsToMatch[i].setPosX(550+i*100);
+				wordsToMatch[i].setPosY(800);
+			}else {
+				wordsToMatch[i].setPosX(mouseX);
+				wordsToMatch[i].setPosY(mouseY);
+			}
+			
+			wordsToMatch[i].drawP(this);
+		}
+		
+		validateDistance();
+		
+		if(buttonControl==4) {
+			fill(40,180,120);
+			rect(700, 820, 150, 50);
+		}
+		//System.out.println(mouseX+":"+mouseY);
+		
+	}
+	//index =2;
+	
+	public void createWord() {
+		//| a | b | c | d | e | < words.size();
+		
+	}	
+	
+	public Palabra[] selectWords() {
+		
+		int x = -1;
+		Palabra[] palabrasAb = new Palabra[4];
+		for (int i = 0; i < palabrasAb.length; i++) {
+			x = (int) random(0,palabritas.size());
+			palabrasAb[i] = new Palabra(palabritas.get(x).getPalabra());
+		}
+		return palabrasAb;
+	}
+	
+	public void mouseClicked() {
+		if(mouseX>500 && mouseY>750) {
+			for (int i = 0; i < wordsToMatch.length; i++) {
+				if(wordsToMatch[i].isCanMove() && dist(mouseX, mouseY, wordsToMatch[i].getPosX(), wordsToMatch[i].getPosY())<30) {
+					wordsToMatch[i].setSelected(true);
+				}
+			}
+		}
+		if(buttonControl==4 && mouseX>700 && mouseX<850 && mouseY>820 && mouseY<870) {
+			saveNewString();
+		}
+		
+		
+	}
+	
+	private void saveNewString() {
+		
+		for (int i = 0; i < palabritas.size(); i++) {
+			if(!palabritas.get(i).isCanMove()) {
+				palabritas.get(i).setPalabra(palabritas.get(i).getPalabra().toUpperCase());
+			}else {
+				palabritas.get(i).setPalabra(palabritas.get(i).getPalabra().toLowerCase());
+			}
+		}
+		
+		String[] newWords = new String[palabritas.size()];
+		
+		for (int i = 0; i < palabritas.size(); i++) {
+			newWords[i] = palabritas.get(i).getPalabra();
+		}
+		
+		
+		saveStrings("data/newWordsFile.txt", newWords);
+	}
+
+	public void mouseReleased() {
+		if(mouseX>500 && mouseY>750) {
+			for (int i = 0; i < wordsToMatch.length; i++) {
+					wordsToMatch[i].setSelected(false);
+			}
+		}
+	}
+	
+	public void validateDistance() {
+		for (int i = 0; i < wordsToMatch.length; i++) {
+			for (int j = 0; j < palabritas.size(); j++) {
+
+				if(wordsToMatch[i].isSelected() && dist(wordsToMatch[i].getPosX(), wordsToMatch[i].getPosY(),
+						palabritas.get(j).getPosX(), palabritas.get(j).getPosY())<15 && 
+						wordsToMatch[i].getPalabra().equals(palabritas.get(j).getPalabra())) {
+					wordsToMatch[i].setSelected(false);
+					wordsToMatch[i].setCanMove(false);
+					//test
+					palabritas.get(j).setCanMove(false);
+					//
+					wordsToMatch[i].setG(200);
+					wordsToMatch[i].setB(100);
+					buttonControl++;
+				}
+			}
+		}
 	}
 } 
--- src/Palabra.java
+++ src/Palabra.java
@@ -3,24 +3,33 @@ import processing.core.PApplet;
 
 public class Palabra {
 
-	private int posX, posY, vel;
+	private int posX, posY, vel, r, g, b;
+	private boolean selected, canMove;
 	private String palabra;
 	
 	public Palabra(String pal) {
 		palabra = pal;
-		posX= (int) (Math.random()*350+10);
+		posX= (int) (Math.random()*940+20);
 		posY= (int) (Math.random()*-450-50);
 		vel= (int) (Math.random()*2+1);
+		selected = false;
+		canMove = true;
+		r=0;
+		g=0;
+		b=0;
 	}
 	
 	public void drawP(PApplet app) {
-		app.fill(0);
+		app.fill(r,g,b);
 		app.text(palabra, posX, posY);
 		
 	}
 	
 	public void mover() {
 		posY+=vel;
+		if(posY>915) {
+			posY = -15;
+		}
 	}
 	
 	public void crearPalabra() {
@@ -29,6 +38,79 @@ public class Palabra {
 		vel= (int) (Math.random()*2+1);
 		//vel = 5;
 	}
+
+	public String getPalabra() {
+		return palabra;
+	}
+
+	public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+
+	public int getVel() {
+		return vel;
+	}
+
+	public void setVel(int vel) {
+		this.vel = vel;
+	}
+
+	public void setPalabra(String palabra) {
+		this.palabra = palabra;
+	}
+
+	public boolean isSelected() {
+		return selected;
+	}
+
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
+
+	public boolean isCanMove() {
+		return canMove;
+	}
+
+	public void setCanMove(boolean canMove) {
+		this.canMove = canMove;
+	}
+
+	public int getR() {
+		return r;
+	}
+
+	public void setR(int r) {
+		this.r = r;
+	}
+
+	public int getG() {
+		return g;
+	}
+
+	public void setG(int g) {
+		this.g = g;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
 	
+		
 	
 }
